f05632c111dfbd758932e137da30559a73e70869
pratikash <pratikashpanda.1234@gmail.com>
2023-05-24 17:12:55 +0530

    cart built

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./styles/app.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Cart from "./components/Cart";
+import {Toaster} from 'react-hot-toast'
 
 function App() {
   return (
@@ -9,8 +11,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart/>} />
       </Routes>
+      <Toaster/>
     </BrowserRouter>
+
   );
 }
 
---
+++ src/components/Cart.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { AiFillDelete } from "react-icons/ai";
+
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+
+const Cart = () => {
+  return (
+    <div className="cart">
+      <main>
+        <CartItem
+          imgSrc={img1}
+          name={"Macbook"}
+          price={1200}
+          qty={1}
+          id="sdfs"
+        />
+      </main>
+      <aside>
+        <h2>Subtotal: ${2000}</h2>
+        <h2>Shipping: ${200}</h2>
+        <h2>Tax: ${20}</h2>
+        <h2>Total: ${2220}</h2>
+      </aside>
+    </div>
+  );
+};
+
+const CartItem = ({
+  imgSrc,
+  name,
+  price,
+  qty,
+  decrement,
+  increment,
+  deleteHandler,
+  id,
+}) => (
+  <div className="cartItem">
+    <img src={imgSrc} alt="" />
+    <article>
+      <h2>{name}</h2>
+      <p>${price}</p>
+    </article>
+    <div>
+      <button onClick={() => decrement(id)}>-</button>
+      <p>{qty}</p>
+      <button onClick={() => increment(id)}>+</button>
+    </div>
+    <AiFillDelete onClick={() => deleteHandler(id)} />
+  </div>
+);
+
+export default Cart;
